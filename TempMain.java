c9ef88f749bc3c93c1b1a5dd60b3c98d11afe86b
thebasement0106 <77373876+thebasement0106@users.noreply.github.com>
2021-02-04 23:20:31 -0700

    Add files via upload

--- TempMain.java
+++ TempMain.java
@@ -1,39 +1,74 @@
 import java.io.*;
+import java.sql.*;
+import java.util.Scanner;
 
 /**
- * This program reads information from a file and displays a user-friendly report based on the data
+ * This program reads information from a database and displays a user-friendly report based on the data
  *
  * Author: Jeff Ostler
  */
 
 public class TempMain {
-    private static final int[][] temps = new int[31][3];
-    private static File inFile = new File("C:\\Users\\Jeff\\Documents\\SLCDecember2020Temperatures.csv");
-    private static File outFile = new File("C:\\Users\\Jeff\\Documents\\TemperaturesReport.txt");
+    private static String[][] temps;
+    private static final File outFile = new File("C:\\Users\\Jeff\\Documents\\TemperaturesReportFromDB.txt");
 
     public static void main(String[] args) throws IOException {
-
         int[] maxHi;
         int[] minLo;
+        int variance;
 
-        //Read information from .csv file
-        if (inFile.exists()){
-            String line;
-            String[] tempArr;
-            int rowCount = 0;
+        // Your database connection information may be different depending on
+        // your MySQL installation and the dbLogin and dbPassword you choose
+        // to use in your database.
+        String connectionString = "jdbc:mysql://127.0.0.1:3306/practice";
+        String dbLogin = "root";
+        String dbPassword = "slccroot";
+        Connection conn = null;
 
-            BufferedReader br = new BufferedReader(new FileReader(inFile));
+        System.out.print("Enter month to view (1-12):");
+        Scanner input = new Scanner(System.in);
+        int month = input.nextInt();
 
-            while((line = br.readLine()) != null) {
-                tempArr = line.split(",");
+        String sql = "SELECT month, day, year, hi, lo FROM temperatures "
+                + "WHERE month = " + month + " AND year = 2020 ORDER BY month, day, year;";
 
+        try
+        {
+            conn = DriverManager.getConnection(connectionString, dbLogin, dbPassword);
+            if (conn != null)
+            {
+                try (Statement stmt = conn.createStatement(
+                        ResultSet.TYPE_SCROLL_INSENSITIVE,
+                        ResultSet.CONCUR_UPDATABLE);
+                     ResultSet rs = stmt.executeQuery(sql))
+                {
+                    int numRows;
+                    int numCols = 5;
+                    rs.last();
+                    numRows = rs.getRow();
 
-                for (int i = 0; i <= tempArr.length - 1; i++) {
-                    temps[rowCount][i] = Integer.parseInt(tempArr[i]);
+                    rs.first();
+                    temps = new String[numRows][numCols];
+                    for (int i = 0; i < numRows; i++)
+                    {
+                        temps[i][0] = rs.getString("month");
+                        temps[i][1] = rs.getString("day");
+                        temps[i][2] = rs.getString("year");
+                        temps[i][3] = rs.getString("hi");
+                        temps[i][4] = rs.getString("lo");
+                        rs.next();
+                    }
                 }
-                rowCount ++;
+                catch (SQLException ex)
+                {
+                    System.out.println(ex.getMessage());
+                }
             }
-            br.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Database connection failed.");
+            e.printStackTrace();
         }
 
         /*
@@ -43,19 +78,22 @@ public class TempMain {
                 "--------------------------------------------------------------\n" +
                 "December 2020: Temperatures in Utah\n" +
                 "--------------------------------------------------------------\n" +
-                "Day  High Low  Variance\n" +
+                "Date       High  Low  Variance\n" +
                 "--------------------------------------------------------------\n";
         System.out.println(outString);
 
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, false);
 
         //Display temperatures for each day in December in tubular format
         for (int i = 0; i <= temps.length - 1; i ++) {
-            outString = String.format("%-5s%-5s%-5s%-5s%n", temps[i][0], temps[i][1], temps[i][2],
-                    (temps[i][1] - temps[i][2]));
+            String date = String.format("%s/%s/%s", temps[i][0], temps[i][1],temps[i][2]);
+
+            variance = Integer.parseInt(temps[i][3]) - Integer.parseInt(temps[i][4]);
+
+            outString = String.format("%s%5s%5s%5s%n", date, temps[i][3], temps[i][4], variance);
 
             System.out.print(outString);
-            writeFile(outFile, outString);
+            writeFile(outFile, outString, true);
         }
 
         maxHi = getMaxHi();
@@ -63,44 +101,47 @@ public class TempMain {
 
         outString = "--------------------------------------------------------------\n";
         System.out.print(outString);
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, true);
 
         //Display the average Hi and Lo and the Max/Min Hi/Lo for the entire month
-        outString = String.format("December Highest Temperature: 12/%d: %d Average Hi: %.1f%n", maxHi[0], maxHi[1], getAverage(1));
+        outString = String.format("December Highest Temperature: %d/%d: %d Average Hi: %.1f%n", month, maxHi[0], maxHi[1], getAverage(3));
         System.out.print(outString);
-        writeFile(outFile, outString);
-        outString = String.format("December Lowest Temperature: 12/%d: %d Average Low: %.1f%n", minLo[0], minLo[1], getAverage(2));
+        writeFile(outFile, outString, true);
+        outString = String.format("December Lowest Temperature: %d/%d: %d Average Low: %.1f%n", month, minLo[0], minLo[1], getAverage(4));
         System.out.print(outString);
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, true);
         outString = "--------------------------------------------------------------\nGraph\n";
         System.out.print(outString);
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, true);
 
         printGraph();
 
         //Graph the max
         for(int i = 0; i <= temps.length - 1; i++){
-            outString = "";
-            System.out.printf("%2d   Hi ", temps[i][0]);
-            outString = String.format("%2d   Hi ", temps[i][0]);
+            int hi = Integer.parseInt(temps[i][3]);
+            int lo = Integer.parseInt(temps[i][4]);
 
-            for (int j = 0; j < temps[i][1]; j++){
+            outString = "";
+            System.out.printf("%2s   Hi ", temps[i][1]);
+            outString = String.format("%2s   Hi ", temps[i][1]);
+
+            for (int j = 0; j < hi; j++){
                 System.out.print("+");
                 outString += "+";
             }
             System.out.println();
             outString += "\n";
-            writeFile(outFile, outString);
+            writeFile(outFile, outString, true);
 
             System.out.print("     Lo ");
             outString = "     Lo ";
-            for (int j = 0; j < temps[i][2]; j++){
+            for (int j = 0; j < lo; j++){
                 System.out.print("-");
                 outString += "-";
             }
             System.out.println();
             outString += "\n";
-            writeFile(outFile, outString);
+            writeFile(outFile, outString, true);
         }
 
         printGraph();
@@ -113,9 +154,11 @@ public class TempMain {
      */
     private static double getAverage(int index){
         double d = 0;
+        int x;
 
         for (int i = 0; i <= temps.length - 1; i ++) {
-            d = d + temps[i][index];
+            x = Integer.parseInt(temps[i][index]);
+            d = d + x;
         }
 
         d = (d / temps.length);
@@ -129,11 +172,16 @@ public class TempMain {
      */
     private static int[] getMaxHi(){
         int[] arr = new int[2];
+        int d;
+        int h;
 
         for (int i = 0; i <= temps.length - 1; i ++) {
-            if (i == 0 || temps[i][1] > arr[1]){
-                arr[0] = temps[i][0];
-                arr[1] = temps[i][1];
+            d = Integer.parseInt(temps[i][1]);
+            h = Integer.parseInt(temps[i][3]);
+
+            if (i == 0 || h > arr[1]){
+                arr[0] = d;
+                arr[1] = h;
             }
         }
         return arr;
@@ -145,11 +193,16 @@ public class TempMain {
      */
     private static int[] getMinLo(){
         int[] arr = new int[2];
+        int d;
+        int m;
 
         for (int i = 0; i <= temps.length - 1; i ++) {
-            if (i == 0 || temps[i][2] < arr[1]){
-                arr[0] = temps[i][0];
-                arr[1] = temps[i][2];
+            d = Integer.parseInt(temps[i][1]);
+            m = Integer.parseInt(temps[i][4]);
+
+            if (i == 0 || m < arr[1]){
+                arr[0] = d;
+                arr[1] = m;
             }
         }
         return arr;
@@ -162,17 +215,17 @@ public class TempMain {
     private static void printGraph() throws IOException {
         String outString = "--------------------------------------------------------------\n";
         System.out.print(outString);
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, true);
 
         outString = String.format("        %-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%n", "1","5","10","15","20","25","30","35","40","45","50");
-        writeFile(outFile, outString);
+        writeFile(outFile, outString,true);
         System.out.print(outString);outString = String.format("        %-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%n", "|","|","|","|","|","|","|","|","|","|","|");
         System.out.print(outString);
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, true);
 
         outString = "--------------------------------------------------------------\n";
         System.out.print(outString);
-        writeFile(outFile, outString);
+        writeFile(outFile, outString, true);
     }
 
     /**
@@ -181,8 +234,8 @@ public class TempMain {
      * @param ln: string representing one line to be written to the file
      * @throws IOException
      */
-    private static void writeFile(File file, String ln) throws IOException {
-        FileWriter fw = new FileWriter(file, true);
+    private static void writeFile(File file, String ln, boolean append) throws IOException {
+        FileWriter fw = new FileWriter(file, append);
         fw.write(ln);
         fw.close();
     }
